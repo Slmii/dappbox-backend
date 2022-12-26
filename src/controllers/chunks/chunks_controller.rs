83ef0339cf6d585f9f76d97a60933af6b7f9194e
Slmii <selami1992@gmail.com>
2022-12-26 10:17:11 +0100

    add call for canister size

--- src/controllers/chunks/chunks_controller.rs
+++ src/controllers/chunks/chunks_controller.rs
@@ -3,7 +3,10 @@ use std::collections::HashMap;
 use candid::{ candid_method, Principal };
 use ic_cdk::{ caller, storage };
 use ic_cdk_macros::{ post_upgrade, pre_upgrade, query, update, init };
-use lib::{ types::{ api_error::ApiError, chunk::{ Chunk, PostChunk } }, utils::{ validate_anonymous, validate_admin } };
+use lib::{
+	types::{ api_error::ApiError, chunk::{ Chunk, PostChunk } },
+	utils::{ validate_anonymous, validate_admin, get_heap_memory_size },
+};
 
 use crate::chunks_store::{ ChunksStore, STATE };
 
@@ -38,6 +41,15 @@ fn get_chunks() -> Result<HashMap<(u32, Principal), Vec<u8>>, ApiError> {
 fn get_chunks_by_chunk_id(chunk_id: u32) -> Result<Vec<u8>, ApiError> {
 	match validate_anonymous(&caller()) {
 		Ok(principal) => ChunksStore::get_chunks_by_chunk_id(chunk_id, principal),
+		Err(err) => Err(err),
+	}
+}
+
+#[query]
+#[candid_method(query)]
+async fn get_size() -> Result<u64, ApiError> {
+	match validate_anonymous(&caller()) {
+		Ok(_) => Ok(get_heap_memory_size()),
 		Err(err) => Err(err),
 	}
 }
--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -1,6 +1,9 @@
 use candid::Principal;
 use crate::{ types::{ asset::{ Asset, AssetType }, api_error::ApiError }, whitelist::whitelist };
 
+#[allow(dead_code)]
+const WASM_PAGE_SIZE: u64 = 65536;
+
 pub fn get_nested_child_assets(assets: &Vec<Asset>, asset_id: &u32) -> Vec<u32> {
 	let mut child_assets: Vec<u32> = vec![];
 
@@ -41,4 +44,26 @@ pub fn validate_anonymous_and_admin(principal: &Principal) -> Result<Principal, 
 	validate_admin(principal)?;
 
 	Ok(*principal)
+}
+
+pub fn get_cycles() -> u64 {
+	#[cfg(target_arch = "wasm32")]
+	{
+		ic_cdk::api::canister_balance()
+	}
+	#[cfg(not(target_arch = "wasm32"))]
+	{
+		0
+	}
+}
+
+pub fn get_heap_memory_size() -> u64 {
+	#[cfg(target_arch = "wasm32")]
+	{
+		(core::arch::wasm32::memory_size(0) as u64) * WASM_PAGE_SIZE
+	}
+	#[cfg(not(target_arch = "wasm32"))]
+	{
+		0
+	}
 }
